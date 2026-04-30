c8f87b5b2fef8382353428583bcc03a269048ecb
Davi <contato@linknacional.com.br>
2026-04-30 20:33:10 -0300

    Refactor event listeners and route handling
    Updated event handling to use new class references and modified route naming.

--- events.php
+++ events.php
@@ -1,11 +1,16 @@
 <?php
 
-declare(strict_types=1);
+//declare(strict_types=1);
 
 use App\CallableEventDispatcherInterface;
 use App\Event;
+use App\Event\BuildRoutes;
+use App\Event\BuildView;
+use DI\Container;
+use Plugin\ExamplePlugin\Controller\HelloWorld;
+//use Plugin\ExamplePlugin\EventHandler\AllTheListeners;
 
-return static function (CallableEventDispatcherInterface $dispatcher) {
+return static function (\App\CallableEventDispatcherInterface $dispatcher) {
     $dispatcher->addListener(
         Event\BuildConsoleCommands::class,
         function (Event\BuildConsoleCommands $event) use ($dispatcher) {
@@ -17,18 +22,19 @@ return static function (CallableEventDispatcherInterface $dispatcher) {
 
     // Tell the view handler to look for templates in this directory too
     $dispatcher->addListener(Event\BuildView::class, function(Event\BuildView $event) {
-        $event->getView()->addFolder('example', __DIR__.'/templates');
+        $event->getView()->addFolder('ExamplePlugin', __DIR__.'/templates');
     });
 
     // Add a new route handled exclusively by the plugin.
     $dispatcher->addListener(Event\BuildRoutes::class, function(Event\BuildRoutes $event) {
         $app = $event->getApp();
 
-        $app->get('/example', \Plugin\ExamplePlugin\Controller\HelloWorld::class)
-            ->setName('example-plugin:index:index')
+        $app->get('/example', HelloWorld::class)
+            ->setName('example:index')
             ->add(\App\Middleware\EnableView::class);
+
     });
 
     // You can also add classes that implement the EventSubscriberInterface
-    $dispatcher->addSubscriber(new \Plugin\ExamplePlugin\EventHandler\AllTheListeners);
+    //$dispatcher->addSubscriber(new AllTheListeners);
 };
