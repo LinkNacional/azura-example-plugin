da78f7e0a0207275b50454428d52418feedb762d
Davi <contato@linknacional.com.br>
2026-04-30 20:35:46 -0300

    Update view rendering in HelloWorld controller

--- src/Controller/HelloWorld.php
+++ src/Controller/HelloWorld.php
@@ -5,15 +5,29 @@ declare(strict_types=1);
 namespace Plugin\ExamplePlugin\Controller;
 
 use App\Controller\SingleActionInterface;
+use App\Exception\Http\InvalidRequestAttribute;
+use App\View;
+use DI\Container;
 use App\Http\Response;
 use App\Http\ServerRequest;
 use Psr\Http\Message\ResponseInterface;
 
 final class HelloWorld implements SingleActionInterface
 {
+    //use EnvironmentAwareTrait;
+    private Container $container;
+    private View $view;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+        $this->view = $container->get(View::class);
+
+    }
+
     public function __invoke(ServerRequest $request, Response $response, array $params): ResponseInterface
     {
         return $request->getView()
-            ->renderToResponse($response, 'example::hello_world');
+            ->renderToResponse($response, 'ExamplePlugin::hello_world');
     }
 }
